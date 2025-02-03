d7be131fd452c8f57f07c2adaf72dd986093cddf
bernardinodamico <B.DAmico@napier.ac.uk>
2025-02-03 17:27:05 +0000

    n/c

--- prova.py
+++ prova.py
@@ -1,9 +1,36 @@
+from pgmpy.inference import VariableElimination
+from pgmpy.models import BayesianNetwork
+import numpy as np
+import pandas as pd
+
+#data = pd.DataFrame(np.random.randint(low=0, high=2, size=(100, 3)), columns=['X', 'Y', 'Z'])
+
+#data.to_csv(path_or_buf="DATA/prova_frequency_data.csv")
+
+data = pd.read_csv(filepath_or_buffer="DATA/prova_frequency_data.csv")
 
 
-def a_function(a: float, b: float)-> float:
-    return a+b
 
 
-c = a_function(a = 4.5, b=6.4)
 
-print(c)
+model = BayesianNetwork([('Z', 'X'), ('Z', 'Y'), ('X', 'Y')])
+model.fit(data)
+inference = VariableElimination(model)
+
+print("_____________________________")
+print("P(Z)")
+phi_query = inference.query(variables=['Z'], evidence=None)
+print(phi_query)
+
+print("_____________________________")
+print("P(X | Z=0)")
+phi_query = inference.query(variables=['X'], evidence={'Z': 0})
+print(phi_query)
+
+print("_____________________________")
+print("P(X | Z=1)")
+phi_query = inference.query(variables=['X'], evidence={'Z': 1})
+print(phi_query)
+
+#phi_query = inference.query(variables=['Y'], evidence=['X', 'Z'])
+#print(phi_query)
