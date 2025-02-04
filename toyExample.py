29ead70c1521c7b5bd9c55e64ee90071cafa5740
bernardinodamico <B.DAmico@napier.ac.uk>
2025-02-04 17:50:31 +0000

    n/c

---
+++ toyExample.py
@@ -0,0 +1,66 @@
+import pyAgrum as gum
+import pandas as pd
+import pyAgrum.causal as csl
+
+'''
+1) Setting up of a Causal Bayesian Network
+'''
+
+# load dataset
+data = pd.read_csv(filepath_or_buffer="DATA/frequency_data_toy_exampe.csv")
+
+# instantiate a (Causal) Bayesian Network
+bn = gum.BayesNet("MyCausalBN")
+
+# add nodes to the (Causal) BN
+
+id_X = bn.add(gum.LabelizedVariable('X', "the intervention variable" , ['True', 'False'])) 
+id_Y = bn.add(gum.LabelizedVariable('Y', "the outcome variable" , ['True', 'False'])) 
+id_Z = bn.add(gum.LabelizedVariable('Z', "a confounder" , ['True', 'False'])) 
+
+#defines edges
+bn.addArc('X', 'Y') # X causes Y
+bn.addArc('Z', 'X')
+bn.addArc('Z', 'Y')
+
+# learn the parameters (i.e. the CPTs) of the BN
+learner = gum.BNLearner(data, bn)
+learner.useSmoothingPrior(1) # Laplace smoothing (e.g. a count C is replaced by C+1)
+bn = learner.learnParameters(bn.dag())
+#----------------------------------------------------------------------------------------
+
+
+'''
+2) Now that we have a Bayesian (causal) net, we can make perform causal effect estimation.
+For this model, the causal effect is estimable via back-door. We apply the back-door formula 
+manually, hence obtaine the estimate via the below line of code, and print out the 
+post-intervetional distribution P(Y | do(X)) for both values of X=0 and X=1.
+'''
+estimate_manual = (bn.cpt('Y') * bn.cpt('Z')).sumOut(['Z'])
+print("    ")
+print("_______________________________________________")
+print("manually estimated P(Y | do(X)):")
+print(estimate_manual)
+
+'''
+3) Now let repeat the same using PyAgrum. First we obtain the estimand (back-door formula)
+and print it in Latex format. Then we print the resulting post-intervetional distribution 
+(estimates) of Y for P(Y | do(X=0)) and P(Y | do(X=1))
+'''
+
+d = csl.CausalModel(bn=bn, latentVarsDescriptor=[("lat", ["X","Z"])])
+res = csl.causalImpact(cm=d, on="Y", doing="X", values={"X":'True'})
+estimate_do_X0 = res[1]
+estimand = res[0]
+
+print("_______________________________________________")
+print(f"PyAgrum estimand formula (in latex format): {estimand.toLatex()}")
+print("_______________________________________________")
+print("Pyagrum estimated P(Y | do(X=True)):")
+print(estimate_do_X0)
+
+res = csl.causalImpact(cm=d, on="Y", doing="X", values={"X":'False'})
+estimate_do_X1 = res[1]
+print("_______________________________________________")
+print("PyAgrum estimated P(Y | do(X=False)):")
+print(estimate_do_X1)
