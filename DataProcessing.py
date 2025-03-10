7a9d33b3d1e9ba731a8658c12dae67c700f8906d
bernardinodamico <B.DAmico@napier.ac.uk>
2025-03-10 17:54:28 +0000

    nc

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,14 +1,46 @@
 import pandas as pd
 from pandas import DataFrame
+import os
 
 class DataProcessing():
 
+    ds_obsrv_vars: DataFrame = None
+
+
+    
+    def initialise_dset_obsrv_vars(self) -> None:
+        self.ds_obsrv_vars = pd.DataFrame()
+
+        fuel_poverty_ds = pd.read_excel(io=os.path.join(os.path.dirname(__file__), r"DATA\RAW\Fuel_poverty_2021.xlsx"),
+                                        sheet_name="fuel_poverty_2021_ukda")
+        
+        self.ds_obsrv_vars.loc[:, 'X'] = fuel_poverty_ds.loc[:, 'WallType']
+        self.ds_obsrv_vars.loc[:, 'V_0'] = fuel_poverty_ds.loc[:, 'DWtype']
+        self.ds_obsrv_vars.loc[:, 'V_2'] = fuel_poverty_ds.loc[:, 'tenure4x']
+        self.ds_obsrv_vars.loc[:, 'V_3'] = fuel_poverty_ds.loc[:, 'DWage']
+        self.ds_obsrv_vars.loc[:, 'V_5'] = fuel_poverty_ds.loc[:, 'Hhsize']
+        self.ds_obsrv_vars.loc[:, 'V_6'] = fuel_poverty_ds.loc[:, 'FloorArea']
+        self.ds_obsrv_vars.loc[:, 'V_7'] = fuel_poverty_ds.loc[:, 'fpfullinc']
+        self.ds_obsrv_vars.loc[:, 'V_8'] = fuel_poverty_ds.loc[:, 'hhcompx']
+
+        self.ds_obsrv_vars.loc[:, 'Mainfueltype'] = fuel_poverty_ds.loc[:, 'Mainfueltype']
+        return 
     
 
-    def initialise_dset_obsrv_vars(self) -> DataFrame:
+    def filter_for_main_fuel_type(self) -> None:
         '''
-        Create ann empy DataFrame with variable Symbols as column headings 
+        Removes instances where mayn fule type for space heating is not "Gas"
         '''
-        df = pd.DataFrame(columns=['X','Y_0','Y_1','W','V_0','V_1','V_2','V_3','V_4','V_5','V_6','V_7','V_8'])
-
-        return df
+        self.ds_obsrv_vars = self.ds_obsrv_vars[self.ds_obsrv_vars.Mainfueltype != 2]
+        self.ds_obsrv_vars = self.ds_obsrv_vars[self.ds_obsrv_vars.Mainfueltype != 3]
+        return
+    
+    
+    def filter_for_income(self) -> None:
+        '''
+        Removes instances where household income is bigger than £100k (as they are lumped all together 
+        above that value) or smaller than £1k
+        '''
+        self.ds_obsrv_vars = self.ds_obsrv_vars[self.ds_obsrv_vars.V_7 > 1000.]
+        self.ds_obsrv_vars = self.ds_obsrv_vars[self.ds_obsrv_vars.V_7 < 99999.]
+        return
--- Scripts__/Build_training_dataset.py
+++ Scripts__/Build_training_dataset.py
@@ -3,7 +3,10 @@ from DataProcessing import DataProcessing
 
 
 dp = DataProcessing()
-training_ds = dp.initialise_dset_obsrv_vars()
+dp.initialise_dset_obsrv_vars()
+dp.filter_for_main_fuel_type()
+dp.filter_for_income()
 
 
+dp.ds_obsrv_vars.to_csv(path_or_buf="DATA/processed_dataset.csv", index=False)
 
